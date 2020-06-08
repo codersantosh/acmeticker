369d02ba012dbe872cd7316253938b6f43870fe5
codersantosh <codersantosh@gmail.com>
2020-06-08 18:02:43 +0545

    Updated : acmeTickerToggle

--- assets/js/acmeticker.js
+++ assets/js/acmeticker.js
@@ -247,7 +247,7 @@
 
 			opts.controls.toggle && opts.controls.toggle.on('click', function(e){
 				mPause = !mPause;
-				thisTicker.toggleClass('acmeticker-pause').trigger('acmeTickerPause', thisTicker);
+				thisTicker.toggleClass('acmeticker-pause').trigger('acmeTickerToggle', thisTicker);
 				if( mPause){
 					thisTicker.stop();
 				}
@@ -305,7 +305,7 @@
 			}
 			opts.controls.toggle && opts.controls.toggle.on('click', function(e){
 				e.preventDefault();
-				thisTicker.toggleClass('acmeticker-pause').trigger('acmeTickerPause', thisTicker);
+				thisTicker.toggleClass('acmeticker-pause').trigger('acmeTickerToggle', thisTicker);
 				isPause = !isPause;
 				restart();
 
